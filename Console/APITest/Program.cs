2a65bc4266d072e98a3775e1e0319aa1b29c9942
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-06-23 07:51:00 +0545

    api test project added

---
+++ Console/APITest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace HelloWorldApplication
+{
+    internal class HelloWorld
+    {
+        private static void Main(string[] args)
+        {
+            var client = new RestClient("https://localhost:7048/api/category/get-all");
+
+            var request = new RestRequest();
+            request.Method = Method.Get;
+            RestResponse response = client.Execute(request);
+            var formatted = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(response.Content), Formatting.Indented);
+            Console.WriteLine(formatted);
+        }
+    }
+}
